0a9ff28591698b9f569d8c8eaf20634757c6a50a
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-07-26 11:41:03 +0200

    Fix: implement query handler

--- organization-service/src/main/java/com/pablodev/organizationservice/organization/application/findAll/FindAllOrganizationsQueryHandler.java
+++ organization-service/src/main/java/com/pablodev/organizationservice/organization/application/findAll/FindAllOrganizationsQueryHandler.java
@@ -2,13 +2,19 @@ package com.pablodev.organizationservice.organization.application.findAll;
 
 import com.pablodev.organizationservice.organization.application.OrganizationsResponse;
 import com.pablodev.shared.domain.query.QueryHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class FindAllOrganizationsQueryHandler implements
         QueryHandler<FindAllOrganizationsQuery, OrganizationsResponse> {
 
+    private final OrganizationAllFinder organizationAllFinder;
+
     @Override
     public OrganizationsResponse handle(FindAllOrganizationsQuery query) {
-        return null;
+        return organizationAllFinder.findAll();
     }
 
 }

edb02b8fd4bc16a6da7693be25f6006f29b8b244
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-07-29 11:43:21 +0200

    Refactor: Rename controller advice

--- organization-service/src/main/java/com/pablodev/organizationservice/organization/infrastructure/controller/OrganizationControllerAdvice.java
+++ organization-service/src/main/java/com/pablodev/organizationservice/organization/infrastructure/controller/OrganizationControllerAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class OrganizationExceptionController {
+public class OrganizationControllerAdvice {
 
     @ExceptionHandler(OrganizationDoesNotExistException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(OrganizationDoesNotExistException ex) {

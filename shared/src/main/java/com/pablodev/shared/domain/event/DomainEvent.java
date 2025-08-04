5ef154984ed6edabe80d2e78dc7d9d5f2a24bd8b
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-08-04 16:02:28 +0200

    Feat: create Domain event

---
+++ shared/src/main/java/com/pablodev/shared/domain/event/DomainEvent.java
@@ -0,0 +1,20 @@
+package com.pablodev.shared.domain.event;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class DomainEvent {
+
+    private String id;
+    private LocalDateTime timestamp;
+
+    protected DomainEvent() {
+        this.id = UUID.randomUUID().toString();
+        this.timestamp = LocalDateTime.now();
+    }
+
+}

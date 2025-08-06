c15dd13d86d1c4a17bb153c5efbb1099d080512a
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-08-06 18:50:56 +0200

    Feat: Add kafka listener dynamically

--- shared/src/main/java/com/pablodev/shared/domain/event/MockOnUserCreated.java
+++ shared/src/main/java/com/pablodev/shared/domain/event/MockOnUserCreated.java
@@ -3,4 +3,8 @@ package com.pablodev.shared.domain.event;
 @DomainSubscriber(MockUserCreatedDomainEvent.class)
 public class MockOnUserCreated {
 
+    public void on(MockUserCreatedDomainEvent event) {
+
+    }
+
 }
--- shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaConfiguration.java
+++ shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaConfiguration.java
@@ -1,6 +1,8 @@
 package com.pablodev.shared.infrastructure.event.kafka;
 
 import com.pablodev.shared.domain.event.DomainEvent;
+import com.pablodev.shared.domain.event.MockOnUserCreated;
+import com.pablodev.shared.domain.event.MockUserCreatedDomainEvent;
 import jakarta.annotation.PostConstruct;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +10,12 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.config.MethodKafkaListenerEndpoint;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @EnableKafka
 @Configuration
@@ -20,19 +25,26 @@ public class KafkaConfiguration {
     private final ApplicationContext applicationContext;
     private final ConsumerFactory<String, DomainEvent> consumerFactory;
     private final KafkaListenerEndpointRegistry registry;
+    private final KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, DomainEvent>> kafkaListenerContainerFactory;
     private final KafkaProperties kafkaProperties;
+    private final MockOnUserCreated subscriber;
 
 
     @PostConstruct
-    public void init() {
+    public void init() throws NoSuchMethodException {
 
-        MethodKafkaListenerEndpoint<String, DomainEvent> methodKafkaListenerEndpoint =
-                new MethodKafkaListenerEndpoint<>();
+        MethodKafkaListenerEndpoint<String, DomainEvent> endpoint = new MethodKafkaListenerEndpoint<>();
 
-        methodKafkaListenerEndpoint.setGroupId(kafkaProperties.getConsumer().getGroupId());
-        methodKafkaListenerEndpoint.setId(UUID.randomUUID().toString());
+        MockUserCreatedDomainEvent event = new MockUserCreatedDomainEvent("example@email.com");
 
-        registry.registerListenerContainer();
+        endpoint.setId(UUID.randomUUID().toString());
+        endpoint.setGroupId(kafkaProperties.getConsumer().getGroupId());
+        endpoint.setAutoStartup(true);
+        endpoint.setTopics(event.getEventName());
+        endpoint.setMessageHandlerMethodFactory(new DefaultMessageHandlerMethodFactory());
+        endpoint.setBean(subscriber);
+        endpoint.setMethod(subscriber.getClass().getMethod("on", MockUserCreatedDomainEvent.class));
+        registry.registerListenerContainer(endpoint, kafkaListenerContainerFactory);
 
     }
 

008ee8dda530478edf51895c942aed51c4d485c4
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-08-11 17:18:36 +0200

    Feat: Create global kafka listener

--- shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaEventBusConfiguration.java
+++ shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaEventBusConfiguration.java
@@ -1,25 +1,13 @@
 package com.pablodev.shared.infrastructure.event.kafka;
 
-import com.pablodev.shared.domain.event.DomainEvent;
-import com.pablodev.shared.infrastructure.event.DomainEventSubscriberInformation;
-import com.pablodev.shared.infrastructure.event.DomainEventSubscribersRegistry;
-import com.pablodev.shared.infrastructure.event.DomainEventsRegistry;
-import jakarta.annotation.PostConstruct;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pablodev.shared.domain.event.MockUserCreatedDomainEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
-import org.springframework.kafka.config.MethodKafkaListenerEndpoint;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 
 
 @Slf4j
@@ -28,53 +16,15 @@ import org.springframework.messaging.handler.annotation.support.DefaultMessageHa
 @RequiredArgsConstructor
 public class KafkaEventBusConfiguration {
 
-    private final ApplicationContext applicationContext;
-    private final KafkaListenerEndpointRegistry registry;
-    private final KafkaProperties kafkaProperties;
-    private final DomainEventSubscribersRegistry subscribersRegistry;
-    private final DomainEventsRegistry domainEventsRegistry;
-    private final KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, DomainEvent>> kafkaListenerContainerFactory;
-    private final DefaultMessageHandlerMethodFactory messageHandlerMethodFactory;
+    private final ObjectMapper objectMapper;
 
-    @PostConstruct
-    public void init() throws NoSuchMethodException {
+    @KafkaListener(topicPattern = ".*")
+    public void consume(String domainEvent) throws JsonProcessingException {
 
-        for (DomainEventSubscriberInformation subscriberInformation : subscribersRegistry.getSubscribersInformation()) {
-            Class<?> subscriber = subscriberInformation.getSubscriber();
-            List<Class<? extends DomainEvent>> events = subscriberInformation.getEvents();
-            registerKafkaListener(subscriber, events);
-        }
+        MockUserCreatedDomainEvent domainEvent1 = objectMapper.readValue(domainEvent,
+                MockUserCreatedDomainEvent.class);
 
-    }
-
-
-    private String[] getEventNames(List<Class<? extends DomainEvent>> events) {
-        return events.stream()
-                .map(domainEventsRegistry::getEventNameOf)
-                .toArray(String[]::new);
-    }
-
-    private Properties createConsumerPropertiesForEvent(Class<? extends DomainEvent> event) {
-        Properties properties = new Properties();
-        properties.setProperty(JsonDeserializer.VALUE_DEFAULT_TYPE, event.getName());
-        return properties;
-    }
-
-    private void registerKafkaListener(Class<?> subscriber, List<Class<? extends DomainEvent>> events)
-            throws NoSuchMethodException {
-
-        MethodKafkaListenerEndpoint<String, DomainEvent> endpoint = new MethodKafkaListenerEndpoint<>();
-        endpoint.setId(UUID.randomUUID().toString());
-        endpoint.setGroupId(kafkaProperties.getConsumer().getGroupId());
-        endpoint.setAutoStartup(true);
-        endpoint.setTopics(getEventNames(events));
-        endpoint.setConsumerProperties(createConsumerPropertiesForEvent(events.getFirst()));
-        endpoint.setMessageHandlerMethodFactory(messageHandlerMethodFactory);
-        endpoint.setBean(applicationContext.getBean(subscriber));
-        endpoint.setMethod(subscriber.getMethod("on", events.getFirst()));
-        registry.registerListenerContainer(endpoint, kafkaListenerContainerFactory, true);
-
-
+        System.out.println(domainEvent);
     }
 
 

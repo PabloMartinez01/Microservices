44254ecbed60b7139436a0a9c048faab94414ff0
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-08-12 19:25:54 +0200

    Feat: Add Kafka consumer registration

---
+++ shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaConsumerRegistration.java
@@ -0,0 +1,19 @@
+package com.pablodev.shared.infrastructure.event.kafka;
+
+import com.pablodev.shared.domain.event.DomainEvent;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class KafkaConsumerRegistration {
+
+    private final Class<?> subscriberClass;
+    private final Class<? extends DomainEvent> eventClass;
+    private String topic;
+    private String groupId;
+    private List<String> partitions;
+
+
+}

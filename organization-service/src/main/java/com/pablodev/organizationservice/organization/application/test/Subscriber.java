8c53201aad5edec4bbcf88f429aeb9535e352ad8
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-08-19 16:50:41 +0200

    Fix: Remove prefix

--- organization-service/src/main/java/com/pablodev/organizationservice/organization/application/test/Subscriber.java
+++
@@ -1,9 +0,0 @@
-package com.pablodev.organizationservice.organization.application.test;
-
-import com.pablodev.shared.domain.event.DomainEvent;
-
-public interface Subscriber {
-
-    public void on(DomainEvent event);
-
-}
--- shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaEventBus.java
+++ shared/src/main/java/com/pablodev/shared/infrastructure/event/kafka/KafkaEventBus.java
@@ -2,7 +2,6 @@ package com.pablodev.shared.infrastructure.event.kafka;
 
 import com.pablodev.shared.domain.event.DomainEvent;
 import com.pablodev.shared.domain.event.EventBus;
-import com.pablodev.shared.infrastructure.event.DomainEventConfiguration;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -13,13 +12,11 @@ import org.springframework.stereotype.Component;
 public class KafkaEventBus implements EventBus {
 
     private final KafkaTemplate<String, DomainEvent> kafkaTemplate;
-    private final DomainEventConfiguration eventConfiguration;
 
     @Override
     public void publish(List<DomainEvent> events) {
         for (DomainEvent event : events) {
-            String topic = "%s.%s".formatted(eventConfiguration.getPrefix(), event.getEventName());
-            kafkaTemplate.send(topic, event);
+            kafkaTemplate.send(event.getEventName(), event);
         }
     }
 

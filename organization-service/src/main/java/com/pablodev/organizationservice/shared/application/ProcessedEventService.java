f8559a0d90cfcd3023bf4873ba023065777452bf
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-09-01 18:18:26 +0200

    Feat: Add service implementation

--- organization-service/src/main/java/com/pablodev/organizationservice/shared/application/ProcessedEventService.java
+++ organization-service/src/main/java/com/pablodev/organizationservice/shared/application/ProcessedEventService.java
@@ -1,5 +1,24 @@
 package com.pablodev.organizationservice.shared.application;
 
+import com.pablodev.organizationservice.shared.domain.ProcessedEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ProcessedEventService {
+
+    private final ProcessedEventRepository repository;
+
+    public void save(String eventId, String subscriberId) {
+        repository.save(eventId, subscriberId);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean hasBeenProcessed(String eventId, String subscriberId) {
+        return repository.count(eventId, subscriberId) > 0;
+    }
 
 }

fc1b4b04f76b994029351ad17c755c511d53c917
Pablo Martinez <pablomartinezcoder@gmail.com>
2025-09-01 18:15:24 +0200

    Feat: Add repository implementation

--- organization-service/src/main/java/com/pablodev/organizationservice/shared/domain/ProcessedEventRepository.java
+++ organization-service/src/main/java/com/pablodev/organizationservice/shared/domain/ProcessedEventRepository.java
@@ -1,5 +1,8 @@
 package com.pablodev.organizationservice.shared.domain;
 
-public class ProcessedEventRepository {
+public interface ProcessedEventRepository {
 
+    void save(String eventId, String subscriberId);
+
+    Integer count(String eventId, String subscriberId);
 }
--- organization-service/src/main/java/com/pablodev/organizationservice/shared/infrastructure/PostgresProcessedEventRepository.java
+++ organization-service/src/main/java/com/pablodev/organizationservice/shared/infrastructure/PostgresProcessedEventRepository.java
@@ -1,5 +1,28 @@
 package com.pablodev.organizationservice.shared.infrastructure;
 
-public class PostgresProcessedEventRepository {
+import com.pablodev.organizationservice.shared.domain.ProcessedEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
+public class PostgresProcessedEventRepository implements ProcessedEventRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void save(String eventId, String subscriberId) {
+        jdbcTemplate.update("""
+                    INSERT INTO processed_events (event_id, subscriber_id) VALUES (?, ?)
+                    ON CONFLICT (event_id, subscriber_id) DO NOTHING
+                """);
+    }
+
+    @Override
+    public Integer count(String eventId, String subscriberId) {
+        return jdbcTemplate.queryForObject("""
+                SELECT COUNT(*) FROM processed_events WHERE event_id=? AND subscriber_id=?
+                """, Integer.class);
+    }
 }
